4d63fc4a08a7746f2330889e74f772a173989a7c
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2022-03-05 15:38:48 +0200

    Add project files.

---
+++ MyMath/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace MyMath;
+
+
+public class Rooter {
+    public double SquareRoot(double input) {
+        if (input <= 0.0) {
+            throw new ArgumentOutOfRangeException(paramName: nameof(input));
+        }
+
+        double result = input;
+        double previousResult = -input;
+
+        while (Math.Abs(previousResult - result) > result / 1000) {
+            previousResult = result;
+            result = (result + input / result) / 2;
+        }
+
+        return result;
+    }
+}
